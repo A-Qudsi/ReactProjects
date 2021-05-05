50635983ce2bce34b669ac6922430c8ba7ff8337
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-05 01:45:12 -0400

    updated forms

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import React, { useState } from 'react'
 import './ExpenseItem.css' 
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card'
 
+
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title); //returns array first value is inital, second value is a function with the changed element. We deconstruct to assign variable
+
+  // we use const because we are not assinging a new value using the = operator. the function handles that. so using const is fine. 
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
 
     return (
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button>Change Title</button>
+        <button onClick={clickHandler}>Change Title</button>
       </Card>
     );
 }
