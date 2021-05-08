1093460faf35b2bde01053747db4f3909b5eab9b
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-08 07:00:51 -0400

    updated modal and App,js

--- 05-Food-Order-App/src/App.js
+++ 05-Food-Order-App/src/App.js
@@ -1,11 +1,12 @@
-import Header from './components/Layout/Header';
 import React, { Fragment } from 'react';
-import Meals from './components/Meals/Meals'
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 const App = () => {
-
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
