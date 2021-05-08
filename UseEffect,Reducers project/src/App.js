3737adbfb66c68c33990c0e2dd6f1c49ccd15837
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-08 04:14:06 -0400

    updated using CTX

--- UseEffect,Reducers project/src/App.js
+++ UseEffect,Reducers project/src/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -16,45 +17,46 @@ function App() {
   // This will check to see if localstorage is already logged in so that we don't have to refresh. However this will cause an infinite loop since setIsLoggedIn will retrigger the useState and it will be an onging thing. this is why we need useEffect.
 
   useEffect(() => {
-    const storedUserLoggedINInformation = localStorage.getItem('isLoggedIn');
+    const storedUserLoggedINInformation = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLoggedINInformation === '1') {
+    if (storedUserLoggedINInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
 
   // because of useEffect we updat the state which triggers only once because our depencies change, the array after the funciton. We don't have an infinite loop since useEffect is only called once when the page initially renders.
 
-
   const loginHandler = (email, password) => {
-    
-
-
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
 
-    // localStorage.setItem('isLoggedIn', '1') 
+    // localStorage.setItem('isLoggedIn', '1')
     // strings could be anything. We are storing data in local stoarge so that we can stay logged in after refresh
 
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
+    localStorage.removeItem("isLoggedIn");
 
     // this removes the localstorage so we can log out on refresh .
-    
+
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
