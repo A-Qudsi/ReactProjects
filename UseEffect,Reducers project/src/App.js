abfb4a4c1d729a228f6750bd2a92e33611868cc9
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-08 02:30:04 -0400

    updated login

--- UseEffect,Reducers project/src/App.js
+++ UseEffect,Reducers project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,43 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // const storedUserLoggedINInformation = localStorage.getItem('isLoggedIn');
+
+  // if (storedUserLoggedINInformation === '1') {
+  //   setIsLoggedIn(true);
+  // }
+
+  // This will check to see if localstorage is already logged in so that we don't have to refresh. However this will cause an infinite loop since setIsLoggedIn will retrigger the useState and it will be an onging thing. this is why we need useEffect.
+
+  useEffect(() => {
+    const storedUserLoggedINInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedINInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  // because of useEffect we updat the state which triggers only once because our depencies change, the array after the funciton. We don't have an infinite loop since useEffect is only called once when the page initially renders.
+
+
   const loginHandler = (email, password) => {
+    
+
+
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // localStorage.setItem('isLoggedIn', '1') 
+    // strings could be anything. We are storing data in local stoarge so that we can stay logged in after refresh
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+
+    // this removes the localstorage so we can log out on refresh .
+    
     setIsLoggedIn(false);
   };
 
--- UseEffect,Reducers project/src/components/Login/Login.js
+++ UseEffect,Reducers project/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,29 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-
-    setFormIsValid(
+  useEffect(() => {
+      setFormIsValid(
       event.target.value.includes('@') && enteredPassword.trim().length > 6
     );
+  }, [enteredEmail, enteredPassword]);
+
+  // add as dependecies as what we're using sidefeffcts in our function. we are using setFormIsvalid(), entered email, enteredpassword. Make sure not to execute the functions. Technically we can ommit setFormIsValid since it will never change since its the second variable in useState() which never changes.
+
+  //Exceptions to what dependecies we add to useEffect. You don't need to add state updating functions. React guarantees that those functions never change -> setFormIsValid, you could but since it never changes it won't do anything.
+  
+  //You also don't need to add "built-in" APIs or functions like .fetch(), localstarge, etc (functions and features built-into the browswer and hence available globally). These browswer APIs/ global functions are not related to the React component render cycle and they also never change
+
+  //You also don't need to add vairables or functions you might've defined OUTSIDE of your components (e.g. if you create a new helper function into a separate file). Such functions or variables are not created inside of a component function and hence changing them won't affect your components (components won't be revaluated if such variables or functions change and vice versa)
+
+  // So long story short. You must add all 'things' you use in your effect function if those 'things' could change because your component (or some parent component ) re-rendered. That's why variables or state defined in component functions, props, or functions defined in component function have to be added as dependencies!
+
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
