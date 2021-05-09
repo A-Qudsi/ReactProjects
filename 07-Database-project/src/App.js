f60b216781e1fc67d74c093e54a403e5bba4ae25
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-09 15:59:11 -0400

    added notes

--- 07-Database-project/src/App.js
+++ 07-Database-project/src/App.js
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
 
-  const [movies, setMovies] = useState([])
-
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films').then(res => {
-      return res.json()
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    })
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
+
+    // function fetchMoviesHandler() {
+    //   fetch("https://swapi.dev/api/films")
+    //     .then((res) => {
+    //       return res.json();
+    //     })
+    //     .then((data) => {
+    //       const transformedMovies = data.results.map((movieData) => {
+    //         return {
+    //           id: movieData.episode_id,
+    //           title: movieData.title,
+    //           openingText: movieData.opening_crawl,
+    //           releaseDate: movieData.release_date,
+    //         };
+    //       });
+    //       setMovies(transformedMovies);
+    //     });
+    // }
+
+    // is the same thing as the code shown above using async and await.
 
   return (
     <React.Fragment>
