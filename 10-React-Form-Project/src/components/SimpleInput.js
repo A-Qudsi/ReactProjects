ffba49089da94d75b578b07a161cd3ce2041f286
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-10 05:38:46 -0400

    updated form

--- 10-React-Form-Project/src/components/SimpleInput.js
+++ 10-React-Form-Project/src/components/SimpleInput.js
@@ -1,15 +1,40 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const nameInputRef = useRef("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid");
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    };
+    
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
 
     console.log(enteredName);
 
@@ -17,23 +42,32 @@ const SimpleInput = (props) => {
     console.log(enteredValue);
 
     setEnteredName("");
-    nameInputRef.current.value = ''; // => Not ideal. don't manipulate the DOM.
+    // nameInputRef.current.value = ''; // => Not ideal. don't manipulate the DOM.
   };
 
   //we use either or the useRef or the useState. the useState keeps track of every keystroke. To reset the field we can just call on setEnteredName and assign it an empty string. With useRef we can do on line 20 however we're manipulating the DOM.
 
+  const nameInputisInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = !nameInputisInvalid
+    ? "form-control"
+    : "form-control invalid";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
+        {nameInputisInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
