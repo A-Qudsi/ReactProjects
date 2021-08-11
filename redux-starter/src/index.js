622a33735faf7cd62f6ecf2e959c3945d2c9be33
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-08-11 16:10:15 -0400

    updated

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp';
+
+let input = "    javascript    "
+
+let output = "div" + input.trim() + "/div"
+
+const trim = str => str.trim();
+// const wrap = (type, str) => `<${type}>${str}</${type}>`;
+const wrap = type => str => `<${type}>${str}</${type}>`;
+
+// const wrapInDiv = str => `<div>${str}</div>`
+// const wrapInSpan = str => `<span>${str}</span>`
+
+const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap("div"))
+transform(input)
+
+const result = wrapInDiv(toLowerCase(trim(input)))
